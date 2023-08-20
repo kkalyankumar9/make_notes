a2b8ceebbbbd6d18e6cb887db0cddea514b62d8c
omkarSw2 <omkar21143@gmail.com>
2023-08-20 19:52:50 +0530

    omkar CRUD

--- be/index.js
+++ be/index.js
@@ -1,18 +1,19 @@
-const express=require("express")
-const {connection}=require("./db")
-const { userRoutes } = require("./routes/userRoutes")
-const app=express()
+const express = require("express");
+const { connection } = require("./db");
+const { userRoutes } = require("./routes/userRoutes");
+const { notesRoutes } = require("./routes/notesRoutes");
+const app = express();
 
-app.use(express.json())
-app.use("/users",userRoutes)
+app.use(express.json());
+app.use("/users", userRoutes);
+app.use("/notes", notesRoutes);
 
-
-app.listen(8080,async()=>{
-    try {
-        await connection
-        console.log("DB connected")
-        console.log("Server is running  port 8080")
-    } catch (error) {
-        console.log(error)
-    }
-})
+app.listen(8080, async () => {
+  try {
+    await connection;
+    console.log("DB connected");
+    console.log("Server is running  port 8080");
+  } catch (error) {
+    console.log(error);
+  }
+});
--- be/routes/userRoutes.js
+++ be/routes/userRoutes.js
@@ -1,77 +1,88 @@
-const express=require("express")
-const bcrypt=require("bcrypt")
-var jwt = require('jsonwebtoken');
-const { UserModel } = require("../models/usermodel")
+const express = require("express");
+const bcrypt = require("bcrypt");
+var jwt = require("jsonwebtoken");
+const { UserModel } = require("../models/usermodel");
+const { BlackListModule } = require("../models/blackList");
 
-const userRoutes=express.Router()
+const userRoutes = express.Router();
 
+userRoutes.post("/signup", async (req, res) => {
+  const { name, email, password } = req.body;
 
-userRoutes.post("/signup",async(req,res)=>{
-    const {name,email,password}=req.body
-   
-    try {
-        const user= await UserModel.find({email})
-    console.log(user)
-    if(user.length){
-       return res.status(400).send({"msg":"User is Already exists"})
-    }else{
-if(validatePassword(password)){
-    console.log(password)
-        bcrypt.hash(password, 2, async(err, hash) =>{
-            // Store hash in your password DB.
-            if(err){
-                res.status(400).send({"err":err})
-            }else{
-                const data=new UserModel({name,email,password:hash})
-               await data.save()
-                res.status(200).send({"msg":"user as Signup"})
-
-            }
+  try {
+    const user = await UserModel.find({ email });
+    console.log(user);
+    if (user.length) {
+      return res.status(400).send({ msg: "User is Already exists" });
+    } else {
+      if (validatePassword(password)) {
+        console.log(password);
+        bcrypt.hash(password, 2, async (err, hash) => {
+          // Store hash in your password DB.
+          if (err) {
+            res.status(400).send({ err: err });
+          } else {
+            const data = new UserModel({ name, email, password: hash });
+            await data.save();
+            res.status(200).send({ msg: "user as Signup" });
+          }
         });
-    }else{
-        res.status(400).send({"msg":"Make Stonge password"})
+      } else {
+        res.status(400).send({ msg: "Make Stonge password" });
+      }
     }
-    }
-    } catch (error) {
-        res.status(400).send({"err":error})
-    }
-})
-userRoutes.post("/signin",async(req,res)=>{
-    const {email,password}=req.body
-    
-    try {
-        const user= await UserModel.findOne({email})
-        console.log(user)
-        if(user){
-            bcrypt.compare(password, user.password, (err, result)=> {
-                // result == true
-                if(result){
-                    const token = jwt.sign({ notes: 'user' }, 'app');
-                    res.status(200).send({"msg":"Login successfull","token":token})
-                }else {
-  
-                    res.status(400).send({"err":"password does not match"}) 
-                }
-            });
-        }else{
-            res.status(400).send({"err":"email not match"})
+  } catch (error) {
+    res.status(400).send({ err: error });
+  }
+});
+userRoutes.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await UserModel.findOne({ email });
+    console.log(user);
+    if (user) {
+      bcrypt.compare(password, user.password, (err, result) => {
+        // result == true
+        if (result) {
+          const token = jwt.sign({ userID: user._id, user: user.name }, "app");
+          res.status(200).send({ msg: "Login successfull", token: token });
+        } else {
+          res.status(400).send({ err: "password does not match" });
         }
-        
-    } catch (error) {
-        res.status(400).send({"err":error})
+      });
+    } else {
+      res.status(400).send({ err: "email not match" });
     }
-})
+  } catch (error) {
+    res.status(400).send({ err: error });
+  }
+});
 
+userRoutes.get("/logout", async (req, res) => {
+  const headers = req.headers.authorization;
+  try {
+    if (headers) {
+      // await BlackListModule.updateMany({}, { $push: { blacklist: [ headers ] } });
+      await BlackListModule.updateMany({}, { $push: { blacklist: [headers] } });
+      console.log(headers);
 
+      res.status(200).send({ msg: " user has Successfully Logout!!" });
+    }
+  } catch (error) {
+    res.status(400).send({ error: error });
+  }
+});
 
-module.exports={userRoutes}
+module.exports = { userRoutes };
 
 //password validation
 
 function validatePassword(password) {
-    // Regular expression pattern
-    const pattern = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~])(?=.{8,})/;
-  
-    // Test the password against the pattern
-    return pattern.test(password);
-  }
+  // Regular expression pattern
+  const pattern =
+    /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~])(?=.{8,})/;
+
+  // Test the password against the pattern
+  return pattern.test(password);
+}
